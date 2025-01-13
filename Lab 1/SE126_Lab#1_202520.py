37224be3fe6c994881c1ac4dbe1611e70c38d69b
estan-neit <estan@email.neit.edu>
2025-01-13 15:49:52 -0500

    SE126.04 lab 1 final

--- Lab 1/SE126_Lab#1_202520.py
+++ Lab 1/SE126_Lab#1_202520.py
@@ -1,3 +1,18 @@
+#Elijah Stan
+#SE 126.04
+#1/13/25
+#
+#Program Prompt Overview:
+#You will be writing one Python file for this project - it is a program that determines whether a
+#meeting room is in violation of fire regulations regarding the maximum room capacity. The
+#program will accept the maximum room capacity and the number of people attending the
+#meeting. If the number of people is less than or equal to the maximum room capacity, the
+#program announces that it is legal to hold the meeting and tells how many additional people may
+#legally attend. If the number of people exceeds the maximum room capacity, the program
+#announces that the meeting cannot be held as planned due to the fire regulation and tells how
+#many people must be excluded in order to meet the fire regulations. The user should be allowed
+#to enter and check as many rooms as they would like without exiting the program.
+#
 #Variable dictionary
 #people: amount of attentees
 #maxCap: maximum capacity the room can hold
