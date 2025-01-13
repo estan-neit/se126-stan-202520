690f5e9431d55902d3071edcec21f48c1fd60676
estan-neit <estan@email.neit.edu>
2025-01-13 00:01:21 -0500

    SE126.04 lab1 Stan

---
+++ Lab 1/SE126_Lab#1_202520.py
@@ -0,0 +1,33 @@
+def difference(people, maxCap):
+
+    return maxCap - people
+
+
+def decision(response):
+
+    response = response.lower()
+    while response != 'y' and response != 'n':
+        response = input("Invalid input. Please enter y or n: ").lower()
+    return response
+
+response = 'y'
+
+while response == 'y':
+
+    meetingName = input("What is the meeting's name? ")
+    print(f"The meeting's name is: {meetingName}")
+
+    maxCap = int(input("What is the room capacity? "))
+    people = int(input("How many people are attending the meeting? "))
+
+    diff = difference(people, maxCap)
+
+    if diff >= 0:
+        print(f"{meetingName} is legal. {diff} more people can be added to {meetingName} and still meet fire regulations.")
+    else:
+        print(f"{meetingName} is illegal. {-diff} people must be removed from {meetingName} to meet fire regulations.")
+
+    anotherCheck = input("Do you have another meeting to check? (y/n): ")
+    response = decision(anotherCheck)
+
+print("Goodbye!")
