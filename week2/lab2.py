2f6fb03d80d2c60fb8547df349f8a56183a01ae6
estan-neit <estan@email.neit.edu>
2025-01-21 00:25:47 -0500

    go

--- week2/lab2.py
+++ week2/lab2.py
@@ -1,15 +1,15 @@
+#Elijah Stan
+#1/20/2025
+#Text File Handling
+
 #W2D2 Lab 2
 
 #PROGRAM PROMPT:
-
-
-#VARIABLE DICTIONARY:
-
+#You have been asked to produce a report that lists all the computers in the csv file
+#filehandling.csv
 
 #--IMPORTS------------------------------------------------------------
 import csv
-#--FUNCTIONS----------------------------------------------------------
-
 #--MAIN EXECUTING CODE------------------------------------------------
 compType = []
 brand = []
@@ -32,38 +32,50 @@ with open("textFile/filehandling.csv") as csvfile:
     file = csv.reader(csvfile)
 
     for rec in file:
+        #count the number of records in the file
         totalRec += 1
         #below code occurs for every record (row) in the file (text file -> 2D list)
         
         #Rec[0] -> computer type
         if rec[0] == "D":
-            compType = "Desktop"
+            compType.append("Desktop")
         else:
-            compType = "Laptop"
+            compType.append("Laptop")
 
-        #Rec[1] -> 
+        #Rec[1] -> brand of computer
         if rec[1] == "DL":
-            brand = "Dell"
-        if rec[1] == "GW":
-            brand = "Gateway"
+            brand.append("Dell")
+        elif rec[1] == "GW":
+            brand.append("Gateway")
+        elif rec[1] == "HP":
+            brand.append("HP")
 
         #assign each field data value to a friendly var name
-        compType = rec[0]
-        brand = rec[1]
-        processor = rec[2]
-        ramGB = rec[3]
-        HDD1Size = rec[4]
-        numDisk = (rec[5])
+        #Rec[2] -> type of processor
+        processor.append(rec[2])
+        #Rec[3] -> amount of RAM in GB
+        ramGB.append(rec[3])
+        #Rec[4] -> size of HDD1
+        HDD1Size.append(rec[4])
+        #Rec[5] -> how many disks the computer has
+        numDisk.append(int(rec[5]))
 
-        if numDisk == 1:
-            HDD2Size = "---"
-            os = rec[6]
-            year = rec[7]
+        if int(rec[5]) == 1:
+            HDD2Size.append("---")
+            #Rec[6] -> type of OS
+            OS.append(rec[6])
+            #Rec[7] -> year of manufacture
+            year.append(rec[7])
         else:
-            HDD2Size = rec[6]
-            os = rec[7]
-            year = int(rec[8])
+            #Rec[6] -> size of HDD2
+            HDD2Size.append(rec[6])
+            #Rec[7] -> type of OS
+            OS.append(rec[7])
+            #Rec[8] -> year of manufacture
+            year.append(int(rec[8]))
 
-
-
-totalRec += 1
+print(f" {'Type':10}  {'Brand':9} {'Processor':10}     {'RAM':5}   {'HDD1':10} {'Disks':6}    {'HDD2':10}  {'OS':10} {'Year':10}")
+print("==============================================================================================")
+print()
+for i in range(totalRec):
+    print(f"{compType[i]:10}   {brand[i]:10}   {processor[i]:10}   {ramGB[i]:5}  {HDD1Size[i]:8} {numDisk[i]:5}       {HDD2Size[i]:10}  {OS[i]:7}    {year[i]:2}")
