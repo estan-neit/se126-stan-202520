c4127b28df096becbbea6904ac845d6b8ba6b57c
estan-neit <estan@email.neit.edu>
2025-01-20 22:56:20 -0500

    lab2

--- week2/lab2.py
+++ week2/lab2.py
@@ -11,6 +11,16 @@ import csv
 #--FUNCTIONS----------------------------------------------------------
 
 #--MAIN EXECUTING CODE------------------------------------------------
+compType = []
+brand = []
+processor = []
+ramGB = []
+HDD1Size = []
+HDD2Size = []
+numDisk = []
+OS = []
+year = []
+
 
 #-----connected to file-------------------------
 
@@ -24,6 +34,18 @@ with open("textFile/filehandling.csv") as csvfile:
     for rec in file:
         totalRec += 1
         #below code occurs for every record (row) in the file (text file -> 2D list)
+        
+        #Rec[0] -> computer type
+        if rec[0] == "D":
+            compType = "Desktop"
+        else:
+            compType = "Laptop"
+
+        #Rec[1] -> 
+        if rec[1] == "DL":
+            brand = "Dell"
+        if rec[1] == "GW":
+            brand = "Gateway"
 
         #assign each field data value to a friendly var name
         compType = rec[0]
@@ -31,16 +53,17 @@ with open("textFile/filehandling.csv") as csvfile:
         processor = rec[2]
         ramGB = rec[3]
         HDD1Size = rec[4]
-        numDisk = rec[5]
-        OS = rec[6]
-        year = rec[7]
+        numDisk = (rec[5])
 
-        if 
-        HDD2Size = rec[6]
-        
+        if numDisk == 1:
+            HDD1Size = "---"
+            os = rec[6]
+            year = rec[7]
+        else:
+            HDD2Size = rec[6]
+            os = rec[7]
+            year = int(rec[8])
 
 
 
-
-
-
+totalRec += 1
