b37d677b8ea2e6bac42290c025defe1c69dc2cfb
estan-neit <estan@email.neit.edu>
2025-03-13 23:47:12 -0400

    final done

--- week9&10_FINAL_project/finalProject.py
+++ week9&10_FINAL_project/finalProject.py
@@ -28,21 +28,29 @@ description = []
 price = []
 # Price of the food item
 
+with open("week9&10_FINAL_project/jewelOfHimalayaMenu.csv") as csvfile: 
+    file = csv.reader(csvfile)
+
+    for rec in file: 
+        course.append(rec[0])
+        name.append(rec[1])
+        description.append(rec[2])
+        price.append(rec[3])
+
 #--DISCONNECTED FROM FILE--------------------------------
 
 def menu():
-    print(f"{'ITEM #':2}|{'COURSE':6}   {'NAME':32}   {'DESCRIPTION':65}  {'PRICE':2}")
-    print("-" * 150)
+    print(f"{'ITEM #':2}|{'COURSE':6}      {'NAME':32}   {'DESCRIPTION':65}  {'PRICE':2}")
+    print("-" * 167)
     for i in range(len(course)):
         print(f" {i + 1:2}    {course[i]:10}  {name[i]:32}   {description[i]:65}  {price[i]:2}")
-    print("-" * 150)
+    print("-" * 167)
 
 
 course = ['Appetizers', 'Appetizers', 'Appetizers', 'Entrees', 'Entrees', 'Entrees', 'Entrees', 'Entrees', 'Entrees', 'Tandoori', 'Tandoori', 'Tandoori', 'Tandoori', 'Rice', 'Rice', 'Noodles', 'Noodles', 'Desserts', 'Desserts']
 name = ['Vegetable Pakoda', 'Lasooni Govi', 'Beef Sekuwa', 'Dal Makhini', 'Chana Massala', 'Himalayan Aloo', 'Murgh Makhini', 'Lamb Vindaloo', 'Saag Paneer', 'Tandoori Chicken (Bones)', 'Tandoori Chicken Tika (Boneless)', 'Seafood Tandoori', 'Paneer Tandoori', 'Lamb Biryani', 'Mix Fried Rice', 'Shrimp Fried Noodle', 'Egg Fried Noodle', 'Gulab Jamun', 'Himalayan Kulfi']
 description = ['Mix vegetable and mashed potatoes dipped in tempura batter and deep fried.', 'Cauliflower battered in chickpea flour and marinated in ginger & garlic & and Himalayan spices.', 'Beef cubes grilled in clay oven.', 'Combination of lentils cooked in tomato and onion and cashew nut sauce. Served with Basmati rice.', 'Chickpeas cooked in onion and tomato & Masala sauce.', 'Spicy potatoes and cauliflower mix in curry sauce. Served with Basmati rice.', 'Chicken marinated in ginger and garlic and tomato onion sauce and Himalayan spices. Served with Basmati rice.', 'Lamb and potato mix curry with spicy sauce. Served with Basmati rice. *Cannot make mild*.', 'Glazed spinach blend with Himalayan cheese (paneer). Served with Basmati rice.', 'Chicken marinated in yogurt and spices and roasted in a tandoor.', 'Chicken marinated in yogurt and spices and roasted in a tandoor.', 'Shrimp and scallops and lobster.', 'Marinated paneer cheese baked in a traditional tandoor oven.', 'Indian style Mutton fried rice served with Raita (yogurt) Sauce.', 'Vegetable & egg & beef & chicken.', 'Stir fried noodles with shrimp and vegetables.', 'Stir fried noodles with egg and vegetables.', 'Deep fried milk balls soaked in sugar syrup.', 'Mango & Pistachio & Cashew & Almond.']
 price = ['8', '12', '13', '20', '18', '19', '23', '24', '22', '20', '22', '36', '23', '24', '20', '24', '17', '5', '5']
-
 
 apps = ['Vegetable Pakoda: Mix vegetable and mashed potatoes dipped in tempura batter and deep fried.  $8', 'Lasooni Govi: Cauliflower battered in chickpea flour and marinated in ginger & garlic & and Himalayan spices  $12', 'Beef Sekuwa: Beef cubes grilled in clay oven  $13']
 entrees = ['Dal Makhini: Combination of lentils cooked in tomato and onion and cashew nut sauce. Served with Basmati rice  $20', 'Chana Massala: Chickpeas cooked in onion and tomato & Masala sauce  $18', 'Himalayan Aloo: Spicy potatoes and cauliflower mix in curry sauce. Served with Basmati rice  $19', 'Murgh Makhini: Chicken marinated in ginger and garlic and tomato onion sauce and Himalayan spices. Served with Basmati rice  $23', 'Lamb Vindaloo: amb and potato mix curry with spicy sauce. Served with Basmati rice. *Cannot make mild*  $24', 'Saag Paneer: Glazed spinach blend with Himalayan cheese (paneer)  $22']
@@ -51,18 +59,20 @@ rice = ['Lamb Biryani: Indian style Mutton fried rice served with Raita (yogurt)
 noodles = ['Shrimp Fried Noodle: Stir fried noodles with shrimp and vegetables  $20', 'Egg Fried Noodle: Stir fried noodles with egg and vegetables  $24']
 desserts = ['Gulab Jamun: Deep fried milk balls soaked in sugar syrup  $5', 'Himalayan Kulfi: Mango & Pistachio & Cashew & Almond  $5']
 
+# Display the menu
 menu()
+# Ask the user if they are ready to place their order
+ans = input("Would you like to place an order? [ y / n ]").lower()
 
-ans = input("Would you like to enter the search program? [ y / n ]").lower()
-
+# Check if the user wants to enter the search program and check for invalid entries
 while ans != "y" and ans != "n":
-    print("***INVALID ENTRY!***")
-    ans = input("Would you like to enter the search program? [ y / n ]").lower()
+    print("Sorry, but we could not understand your response. Please try again.")
+    ans = input("Would you like to place an order? [ y / n ]").lower()
 
 # Online menu
 while ans == "y":
     # ans: user's choice to start search system
-    print(f"\tHere is our menu:")
+    print(f"\tHere are your search options:")
     print("==========================================")
     print("\t~Jewel of Himalaya Online Menu~")
     print("1. Search by appetizers")
@@ -94,15 +104,9 @@ while ans == "y":
             print("Check your cAsInG and sPeLlInG and try again!")
         else:
             # Appetizer FOUND!
-            print(f"Your search for {searchApp} was FOUND! Here is their data: ")
+            print(f"Your search for {searchApp} was FOUND! Here is more about the appetizer: ")
             for index in found:
                 print(f"{apps[index]:3}")
-
-                input("Would you like to order this item? [y/n]: ").lower()
-                if ans == "y":
-                    print("Your order has been placed!")
-                if ans == "n":
-                    print("Returning you back to the menu.")
 
     elif searchType == "2":
         found = [] # Flag var, will be replaced with index position if name is found
@@ -125,7 +129,7 @@ while ans == "y":
             print("Check your cAsInG and sPeLlInG and try again!")
         else:
             # Entree FOUND!
-            print(f"Your search for {searchEntree} was FOUND! Here is their data: ")
+            print(f"Your search for {searchEntree} was FOUND! Here is more about the entree: ")
             for index in found:
                 print(f"{entrees[index]:3}")
 
@@ -150,7 +154,7 @@ while ans == "y":
             print("Check your cAsInG and sPeLlInG and try again!")
         else:
             # Tandoori FOUND!
-            print(f"Your search for {searchTandoori} was FOUND! Here is their data: ")
+            print(f"Your search for {searchTandoori} was FOUND! Here is more about the tandoori dish: ")
             for index in found:
                 print(f"{tandoori[index]:3}")
         
@@ -175,7 +179,7 @@ while ans == "y":
             print("Check your cAsInG and sPeLlInG and try again!")
         else:
             # Rice FOUND!
-            print(f"Your search for {searchRice} was FOUND! Here is their data: ")
+            print(f"Your search for {searchRice} was FOUND! Here is more about the rice dish: ")
             for index in found:
                 print(f"{rice[index]:3}")
     
@@ -201,7 +205,7 @@ while ans == "y":
             print("Check your cAsInG and sPeLlInG and try again!")
         else:
             # Noodles FOUND!
-            print(f"Your search for {searchNoodles} was FOUND! Here is their data: ")
+            print(f"Your search for {searchNoodles} was FOUND! Here is more about the noodle dish: ")
             for index in found:
                 print(f"{noodles[index]:3}")
             
@@ -226,7 +230,7 @@ while ans == "y":
             print("Check your cAsInG and sPeLlInG and try again!")
         else:
             # Dessert FOUND!
-            print(f"Your search for {searchDessert} was FOUND! Here is their data: ")
+            print(f"Your search for {searchDessert} was FOUND! Here is more about the dessert: ")
             for index in found:
                 print(f"{desserts[index]:3}")
 
@@ -237,8 +241,6 @@ while ans == "y":
         ans = "n"
 
     else:
-        print("ERROR: Invalid search type. Please try again.")
+        print("ERROR: Invalid search. Please try again.")
 
     answer(ans)
-
-
